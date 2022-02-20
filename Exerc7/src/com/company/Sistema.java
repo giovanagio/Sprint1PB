bbf9ddc47c096124fdbc53d09eda82facfa5df10
Felype <felypeee@live.com>
2022-02-19 23:58:43 -0300

    Exercício 7 finalizado

--- Exerc7/src/com/company/Sistema.java
+++ Exerc7/src/com/company/Sistema.java
@@ -8,11 +8,12 @@ public class Sistema {
 
     public static void main(String[] args) {
 
-
+        //Buscando a hora atual e fazendo sua transformação para inteiro
         Date dataHoraAtual = new Date();
         String horarioAgora = new SimpleDateFormat("HH").format(dataHoraAtual);
         int horaParaUtilizar = Integer.parseInt(horarioAgora);
 
+        //Cadastrando 2 usuários no sistema de cadastro para login
         Scanner sc = new Scanner(System.in);
         List<String> usuariosCadastrados = new ArrayList<>();
         usuariosCadastrados.add("compass");
@@ -21,15 +22,17 @@ public class Sistema {
         senhasCadastradas.add("12345");
         senhasCadastradas.add("010203");
 
-
-        //adicionando login
-
-        System.out.println("Digite seu login");
+        //Pedindo ao usuário login e senha para entrar no sistema
+        System.out.println("-------------------------");
+        System.out.println("SISTEMA COMPASS DE ACESSO");
+        System.out.println("-------------------------");
+        System.out.println("Digite seu login:");
         String usuarioLogin = sc.next().toLowerCase();
-        System.out.println("Digite sua senha");
+        System.out.println("Digite sua senha:");
         String usuarioSenha = sc.next().toLowerCase();
 
 
+        //Fazendo a comparação de usuario e senha corretos e anunciando o horário de acesso, posteriormente.
         for (int i = 0; i < usuariosCadastrados.size(); i++) {
 
             if(usuarioLogin.equals(usuariosCadastrados.get(i))&& usuarioSenha.equals(senhasCadastradas.get(i))) {
@@ -41,16 +44,16 @@ public class Sistema {
                     System.out.println("Boa tarde, você se logou ao nosso sistema");
                     break;
                 } else if (horaParaUtilizar >= 18 && horaParaUtilizar < 24) {
-                    System.out.println("“Boa noite, você se logou ao nosso sistema.");
+                    System.out.println("Boa noite, você se logou ao nosso sistema.");
                     break;
                 } else if (horaParaUtilizar >= 0 && horaParaUtilizar < 6) {
-                    System.out.println("“Boa madrugada, você se logou ao nosso sistema.");
+                    System.out.println("Boa madrugada, você se logou ao nosso sistema.");
                     break;
                 }
             }
-         if(i == (usuariosCadastrados.size()-1)) {
+            if(i == (usuariosCadastrados.size()-1)) {
             System.out.println("Usuário e/ou senha incorretos.");
-        }
+            }
 
         }
     }
